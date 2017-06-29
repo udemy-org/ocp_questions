3068af366e493f03eabb334dea2f78c3686451d0
Pep Mendez <jmendez1@xtec.cat>
2017-06-29 10:11:40 +0200

    Question 1/3 in progress

---
+++ ocp_questions/src/org/escoladeltreball/ocp_questions/seccion01/Libro.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.escoladeltreball.ocp_questions.seccion01;
+
+/**
+ * @author jmendez
+ *
+ */
+public class Libro {
+	private int ISBN;
+	private String titulo;
+	private String[] autores;
+	private String editorial;
+
+	public int hashCode() {
+		return ISBN;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Libro)) {
+			return false;
+		}
+		Libro other = (Libro) obj;
+		return this.ISBN == other.ISBN;
+	}
+	// getters/setters
+}
--- ocp_questions/src/org/escoladeltreball/ocp_questions/seccion01/Main05.java
+++ ocp_questions/src/org/escoladeltreball/ocp_questions/seccion01/Main05.java
@@ -7,26 +7,6 @@ package org.escoladeltreball.ocp_questions.seccion01;
  * @author jmendez
  *
  */
-class Book {
-	private int ISBN;
-	private String title, author;
-	private int pageCount;
-
-	public int hashCode() {
-		return ISBN;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof Book)) {
-			return false;
-		}
-		Book other = (Book) obj;
-		return this.ISBN == other.ISBN;
-	}
-	// imagine getters and setters are here
-}
-
 public class Main05 {
 
 	/**
